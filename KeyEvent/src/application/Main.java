b93481d5fcbf402a64a293fd75bfa51e4cf73d25
hello0a <aya70713@gmail.com>
2025-11-06 12:19:09 +0900

    ~ 써클 이동(3주차)

--- KeyEvent/src/application/Main.java
+++ KeyEvent/src/application/Main.java
@@ -1,16 +1,18 @@
 package application;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	
-	private static Scene scene;
+	public static Scene scene;
 	public static Stage stage;
 	/**
 	 * 화면 시작 메서드
@@ -18,15 +20,31 @@ public class Main extends Application {
 	 */
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		Map<String, Object> map = get("Main");
 		stage = primaryStage;
 		// Main.fxml 화면 파일을 로드
-		Parent container = loadFXML("Main");
+		Parent container = (Parent) map.get("root");
+		// Controller 가져오기
+		MainController mainController = (MainController) map.get("controller");
 		// Main.fxml 화면 레이아웃을 지정하여 Scene 생성
 		scene = new Scene (container);
+		//키 이벤트 등록
+		mainController.keyEvent();
 		// Stage 에 Scene 등록
 		stage.setScene(scene);
 		// Stage 출력
 		stage.show();
+	}
+	
+	public static Map<String, Object> get (String fxml) throws Exception {
+		FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxml + ".fxml"));
+		Parent root = fxmlLoader.load();
+		Object controller = fxmlLoader.getController();
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("loader", fxmlLoader);
+		map.put("root", root);
+		map.put("controller", controller);
+		return map;
 	}
 	/**
 	 * Scene 에 FXML로 불러온 컨테이너를 지정하는 메서드
