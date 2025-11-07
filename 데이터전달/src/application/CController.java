5f73e6aa11848a6803a4e9de1346870f49d22946
hello0a <aya70713@gmail.com>
2025-11-07 12:06:10 +0900

    ~ 데이터전달 (3주차)

--- 데이터전달/src/application/CController.java
+++ 데이터전달/src/application/CController.java
@@ -1,5 +1,6 @@
 package application;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
@@ -12,22 +13,31 @@ public class CController implements DataReceiver{
     
     @FXML
     void initialize() {
+    	// 1번
     	System.out.println("# initialize #");
-    	System.out.println("receiveData : " + receiveData);
+    	System.out.println("receiveData : " + this.receiveData);
+    	
+    	// 3번
+    	Platform.runLater( () -> {
+    		System.out.println("# runlater() #");
+    		System.out.println("receiveData : " + this.receiveData);
+    	});
     }
 
-    @FXML
-    void to(ActionEvent event) {
-
-    }
-
+    // 2번
 	@Override
 	public void receiveData(Object data) {
 		if ( data instanceof String text ) {
 			input.setText( text );			
 		}
 		this.receiveData = data;
+		System.out.println("# receiveData() #");
 		System.out.println("receiveData : " + receiveData);
+	}
+	
+	@FXML
+	void to(ActionEvent event) {
+		
 	}
 
 }
